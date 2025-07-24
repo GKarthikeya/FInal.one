d049aacef22f364e33669682f5ec19db514cb53d
GKarthikeya <128343681+GKarthikeya@users.noreply.github.com>
2025-07-24 22:06:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y wget unzip
+apt-get install -y wget unzip fonts-liberation libasound2 libatk-bridge2.0-0 \
+    libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 \
+    libnspr4 libnss3 libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2 \
+    xdg-utils
 
-# Install Chrome (stable version)
+# Download and install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
 
+# Python deps
 pip install -r requirements.txt
