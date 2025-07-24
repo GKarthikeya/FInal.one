fb43f8833a28ff84da2f8d67ce9fc4ad555add54
GKarthikeya <128343681+GKarthikeya@users.noreply.github.com>
2025-07-24 21:51:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y chromium
+apt-get install -y wget unzip
+
+# Install Chrome (stable version)
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 pip install -r requirements.txt
